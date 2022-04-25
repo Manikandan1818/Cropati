6b9cdac7b1843659c85e375801842eaf07b0e8f1
Manikandan1818 <simbumanii18@gmail.com>
2022-04-25 23:26:25 +0530

    Cropati app createdgit add .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,157 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useMemo, useState } from "react";
+import "./app.css";
+import Quiz from "./components/Quiz";
+import Start from "./components/Start";
+import { Timer } from "./Timer";
 
 function App() {
+  const [username, setUsername] = useState(null);
+  const [questionNumber, setQuestionNumber] = useState(1);
+  const [stop, setStop] = useState(false);
+  const [earned, setEarned] = useState("₹ 0");
+
+  const data = [
+    {
+      id: 1,
+      question: "Rolex is a company that specializes in what type of product?",
+      answers: [
+        {
+          text: "Phone",
+          correct: false,
+        },
+        {
+          text: "Watches",
+          correct: true,
+        },
+        {
+          text: "Food",
+          correct: false,
+        },
+        {
+          text: "Cosmetic",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 2,
+      question: "When did the website `Facebook` launch?",
+      answers: [
+        {
+          text: "2004",
+          correct: true,
+        },
+        {
+          text: "2005",
+          correct: false,
+        },
+        {
+          text: "2006",
+          correct: false,
+        },
+        {
+          text: "2007",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 3,
+      question: "Who played the character of harry potter in movie?",
+      answers: [
+        {
+          text: "Johnny Deep",
+          correct: false,
+        },
+        {
+          text: "Leonardo Di Caprio",
+          correct: false,
+        },
+        {
+          text: "Denzel Washington",
+          correct: false,
+        },
+        {
+          text: "Daniel Red Cliff",
+          correct: true,
+        },
+      ],
+    },
+  ];
+  const moneyPrymid = useMemo(
+    () =>
+      [
+        { id: 1, amount: "₹ 100" },
+        { id: 2, amount: "₹ 500" },
+        { id: 3, amount: "₹ 1000" },
+        { id: 4, amount: "₹ 5000" },
+        { id: 5, amount: "₹ 10000" },
+        { id: 6, amount: "₹ 25000" },
+        { id: 7, amount: "₹ 50000" },
+        { id: 8, amount: "₹ 75000" },
+        { id: 9, amount: "₹ 100000" },
+        { id: 10, amount: "₹ 500000" },
+        { id: 11, amount: "₹ 1000000" },
+        { id: 12, amount: "₹ 2500000" },
+        { id: 13, amount: "₹ 5000000" },
+        { id: 14, amount: "₹ 7500000" },
+        { id: 15, amount: "₹ 10000000" },
+      ].reverse(),
+    []
+  );
+
+  useEffect(() => {
+    questionNumber > 1 &&
+      setEarned(moneyPrymid.find((m) => m.id === questionNumber - 1).amount);
+  }, [moneyPrymid, questionNumber]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {username ? (
+        <>
+          <div className="main">
+            {stop ? (
+              <h1 className="endText">
+                {username} earned : {earned}
+              </h1>
+            ) : (
+              <>
+                <div className="top">
+                  <div className="timer">
+                    <Timer setStop={setStop} questionNumber={questionNumber} />
+                  </div>
+                </div>
+                <div className="bottom">
+                  <Quiz
+                    data={data}
+                    setStop={setStop}
+                    questionNumber={questionNumber}
+                    setQuestionNumber={setQuestionNumber}
+                  />
+                </div>
+              </>
+            )}
+          </div>
+          <div className="side">
+            <ul className="moneyList">
+              {moneyPrymid.map((m) => (
+                <li
+                  className={
+                    questionNumber === m.id
+                      ? "moneyListItem active"
+                      : "moneyListItem"
+                  }
+                >
+                  <span className="moneyListItemNumber">{m.id}</span>
+                  <span className="moneyListItemAmount">{m.amount}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </>
+      ) : (
+        <Start setUsername={setUsername} />
+      )}
     </div>
   );
 }
