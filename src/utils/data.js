d55d7b60bce22fcd08f63d783ac9278a678a706f
Manikandan1818 <simbumanii18@gmail.com>
2022-04-26 20:43:48 +0530

    cropati app completed!

---
+++ src/utils/data.js
@@ -0,0 +1,336 @@
+export const data = [
+  {
+    id: 1,
+    question: "First Afghan War took place in",
+    answers: [
+      {
+        text: "1839",
+        correct: true,
+      },
+      {
+        text: "1848",
+        correct: false,
+      },
+      {
+        text: "1843",
+        correct: false,
+      },
+      {
+        text: "1833",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 2,
+    question: "When did the website `Facebook` launch?",
+    answers: [
+      {
+        text: "2004",
+        correct: true,
+      },
+      {
+        text: "2005",
+        correct: false,
+      },
+      {
+        text: "2006",
+        correct: false,
+      },
+      {
+        text: "2007",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 3,
+    question: "Who played the character of harry potter in movie?",
+    answers: [
+      {
+        text: "Johnny Deep",
+        correct: false,
+      },
+      {
+        text: "Leonardo Di Caprio",
+        correct: false,
+      },
+      {
+        text: "Denzel Washington",
+        correct: false,
+      },
+      {
+        text: "Daniel Red Cliff",
+        correct: true,
+      },
+    ],
+  },
+
+  {
+    id: 4,
+    question:
+      "Which one of the following river flows between Vindhyan and Satpura ranges?",
+    answers: [
+      {
+        text: "Narmada",
+        correct: true,
+      },
+      {
+        text: "Mahanadi",
+        correct: false,
+      },
+      {
+        text: "Son",
+        correct: false,
+      },
+      {
+        text: "Netravati",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 5,
+    question: "The Central Rice Research Station is situated in?",
+    answers: [
+      {
+        text: "Chennai",
+        correct: true,
+      },
+      {
+        text: "Cuttack",
+        correct: false,
+      },
+      {
+        text: "Bangalore",
+        correct: false,
+      },
+      {
+        text: "Quilon",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 6,
+    question: "Who among the following wrote Sanskrit grammar?",
+    answers: [
+      {
+        text: "Kalidasa",
+        correct: false,
+      },
+      {
+        text: "Charak",
+        correct: false,
+      },
+      {
+        text: "Panini",
+        correct: true,
+      },
+      {
+        text: "Aryabhatt",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 7,
+    question: "Which among the following headstreams meets the Ganges in last?",
+    answers: [
+      {
+        text: "Alaknanda",
+        correct: false,
+      },
+      {
+        text: "Pindar",
+        correct: false,
+      },
+      {
+        text: "Mandakini",
+        correct: false,
+      },
+      {
+        text: "Bhagirathi",
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 8,
+    question: "The metal whose salts are sensitive to light is?",
+    answers: [
+      {
+        text: "Zinc",
+        correct: false,
+      },
+      {
+        text: "Silver",
+        correct: true,
+      },
+      {
+        text: "Copper",
+        correct: false,
+      },
+      {
+        text: "Aluminum",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 9,
+    question: "Patanjali is well known for the compilation of –",
+    answers: [
+      {
+        text: "Yoga Sutra",
+        correct: true,
+      },
+      {
+        text: "Panchatantra",
+        correct: false,
+      },
+      {
+        text: "Brahma Sutra",
+        correct: false,
+      },
+      {
+        text: "Ayurveda",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 10,
+    question:
+      "River Luni originates near Pushkar and drains into which one of the following?",
+    answers: [
+      {
+        text: "Rann of Kachchh",
+        correct: true,
+      },
+      {
+        text: "Arabian Sea",
+        correct: false,
+      },
+      {
+        text: "Gulf of Cambay",
+        correct: false,
+      },
+      {
+        text: "Lake Sambhar",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 11,
+    question: "The country that has the highest in Barley Production?",
+    answers: [
+      {
+        text: "China",
+        correct: false,
+      },
+      {
+        text: "India",
+        correct: false,
+      },
+      {
+        text: "Russia",
+        correct: true,
+      },
+      {
+        text: "France",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 12,
+    question: "Tsunamis are not caused by",
+    answers: [
+      {
+        text: "Hurricanes",
+        correct: true,
+      },
+      {
+        text: "Earthquakes",
+        correct: false,
+      },
+      {
+        text: "Undersea landslides",
+        correct: false,
+      },
+      {
+        text: "Volcanic eruptions",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 13,
+    question: "Where was the electricity supply first introduced in India?",
+    answers: [
+      {
+        text: "Mumbai",
+        correct: false,
+      },
+      {
+        text: "Dehradun",
+        correct: false,
+      },
+      {
+        text: "Darjeeling",
+        correct: true,
+      },
+      {
+        text: "Chennai",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 14,
+    question: "Friction can be reduced by changing from",
+    answers: [
+      {
+        text: "rolling to sliding",
+        correct: false,
+      },
+      {
+        text: "sliding to rolling",
+        correct: true,
+      },
+      {
+        text: "dynamic to static",
+        correct: false,
+      },
+      {
+        text: "potential energy to kinetic energy",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 15,
+    question:
+      "At which one of the following places do the rivers Alaknanda and Bhagirathi merge to form Ganga?",
+    answers: [
+      {
+        text: "Devprayag",
+        correct: true,
+      },
+      {
+        text: "Rudra Prayag",
+        correct: false,
+      },
+      {
+        text: "Karnaprayag",
+        correct: false,
+      },
+      {
+        text: "Vishnuprayag",
+        correct: false,
+      },
+    ],
+  },
+];
